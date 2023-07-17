4deccdc99e85f3d64c38b15614e36b117e63e059
ye0ngeun <duddms2415@naver.com>
2023-07-17 21:30:19 +0900

    test : 회원가입 테스트 추가

---
+++ src/main/java/hello/Core/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.Core;
+
+import hello.Core.member.Grade;
+import hello.Core.member.Member;
+import hello.Core.member.MemberService;
+import hello.Core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        System.out.println("new Member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
